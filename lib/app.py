0e41d4dd5065c27d4b84752bc05ccf3bf358f45b
philip baya mweri <dukebaya@gmail.com>
2023-09-25 20:37:22 +0300

    Summation of 1 to 21

--- lib/app.py
+++ lib/app.py
@@ -1,5 +1,18 @@
-print("Hello World!")
 print('Hello World!')
-print('This is it!')
-print('I love moringa')
-print('I love moringa')
+
+j = 0
+for i in range(1,21) : 
+    if i % 2 == 0 : 
+        continue
+    j += i
+print(j)
+
+x = 7
+if (x % 3 == 0) and (x % 5 == 0):
+    print("FizzBuzz")
+elif x % 3 == 0:
+    print("Fizz")
+elif x % 5 == 0:
+    print("Buzz")
+else:
+    print(x)
