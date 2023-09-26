e1f364c984bff275ef08e74b96f3e20344711b71
philip baya mweri <dukebaya@gmail.com>
2023-09-26 21:06:15 +0300

    More code tests

--- lib/app.py
+++ lib/app.py
@@ -16,3 +16,31 @@ elif x % 5 == 0:
     print("Buzz")
 else:
     print(x)
+
+    n = 100
+total_sum = 0
+counter = 1
+while counter <= n:
+  total_sum += counter
+  counter += 1
+print("Sum of 1 until " + str(n) + " results in " + str(total_sum))
+
+print("hello World!")
+
+for number in range(1,6):
+    print(number)
+
+string = "Hello!"
+for c in string:
+    print(c)
+
+fruits = ['apple', 'banana', 'cherry']
+for f in fruits:
+    print(f)
+
+Movies = ( 'Rambo', 'Recruit', 'Condor')
+print(Movies)
+
+String = "hello!"
+for c in String :
+ print(c)
